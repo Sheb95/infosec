aa9a9daf859162bde79fe83d6b20be0b2f6d4846
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-04-28 20:12:00 +0100

    required and installed helmet

--- myApp.js
+++ myApp.js
@@ -1,61 +1,6 @@
-const express = require('express');
+const express = require("express");
+//helmet goes here
+const helmet = requite("helmet");
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+app.use(helmet());
